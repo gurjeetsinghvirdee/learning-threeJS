54d0a72a6a7d9bf178e7ae84717deec16d9d1d28
Gurjeet Singh Virdee <73753957+gurjeetsinghvirdee@users.noreply.github.com>
2023-09-10 00:51:34 +0530

    created a new function that added some stars in the background

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ camera.position.setZ(30);
 renderer.render( scene, camera );
 
 const geometry = new THREE.TorusGeometry( 10, 3, 16, 100 );
-const material = new THREE.MeshStandardMaterial( { color: 0x0096FF } );
+const material = new THREE.MeshStandardMaterial( { color: 0xff4500 } );
 const torus = new THREE.Mesh( geometry, material );
 
 scene.add(torus)
@@ -55,3 +55,18 @@ function animate() {
 }
 
 animate();
+
+
+// Function that add stars in the background
+function addStar() {
+    const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+    const material = new THREE.MeshStandardMaterial( { color: 0xffffff } );
+    const star = new THREE.Mesh( geometry, material );
+
+    const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread( 100 ) );
+
+    star.position.set(x, y, z);
+    scene.add(star)
+}
+
+Array(200).fill().forEach(addStar);
